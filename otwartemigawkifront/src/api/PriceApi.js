ec4080eb808bdd40f18974a5dee3e534908db9dc
Kuboss949 <jakub.lopataa01@gmail.com>
2024-05-13 12:16:27 +0200

    jwtCookie auth

--- otwartemigawkifront/src/api/PriceApi.js
+++ otwartemigawkifront/src/api/PriceApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const fetchSessionTypes = async () => {
     try {
-        const response = await axios.get('http://localhost:8080/api/session/all');
+        const response = await axios.get('/api/session/all');
         return response.data;
     } catch (error) {
         throw new Error('Error fetching data:', error);
